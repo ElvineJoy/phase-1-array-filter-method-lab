d3542eb821f69b922880ea2adf3be735a2565c2f
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-23 15:12:09 +0300

    Filter lab completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+function findMatching (drivers, string) {
+    return drivers.filter((driverMatch) => driverMatch.toLowerCase() === string.toLowerCase());
+};
+
+// function fuzzy match
+
+function fuzzyMatch (drivers, enteredString) {
+    return drivers.filter((driverMatch) => driverMatch.toLowerCase().indexOf(enteredString.toLowerCase())=== 0);
+};
+
+// function matchName
+
+function matchName (drivers, thirdString) {
+    return drivers.filter((driverMatch) => driverMatch.name === thirdString )
+}
